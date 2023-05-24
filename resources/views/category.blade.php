6953eff9b880541e933e2cf14fd956bb8e059a0b
dev-naf <ahnaf.fattah@outlook.com>
2023-05-24 15:01:01 +0700

    edit page category

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -11,11 +11,10 @@
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  {{-- <link href="/your-path-to-fontawesome/css/fontawesome.css" rel="stylesheet">
-  <link href="/your-path-to-fontawesome/css/brands.css" rel="stylesheet">
-  <link href="/your-path-to-fontawesome/css/solid.css" rel="stylesheet">
-  <link href="https://fontawesome.com/search?o=r&m=free&s=regular&f=classic"> --}}
-
+  {{-- FontAwesome Icon --}}
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  {{-- Bootstrap Icon --}}
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   
 </head>
 <body>
@@ -27,6 +26,7 @@
     <a href="welcome.blade.php" class="logo d-flex align-items-center">
       <span class="d-none d-lg-block"> Assalisa Beauty Clinic</span>
     </a>
+    
     <i class="bi bi-list toggle-sidebar-btn"></i>
   </div>
 
@@ -188,6 +188,8 @@
       {{-- Log Out --}}
       <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="icons-nav" data-bs-toggle="collapse" href="#">
-    
+
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js">
+    </script>
 </body>
 </html>
