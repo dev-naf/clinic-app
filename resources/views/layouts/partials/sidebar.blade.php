3ed3f9c31310b0e76a1106a187a86804eed203c6
dev-naf <ahnaf.fattah@outlook.com>
2023-05-24 15:48:54 +0700

    update layout

---
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -0,0 +1,98 @@
+<nav id="sidebarMenu" class="col-md-3 mt-0 col-lg-2 col-xl-2 d-md-block  sidebar collapse sidebar-custom">
+    <div class="position-sticky pt-2 sidebar-sticky ">
+        <ul class="sidebar-nav" id="sidebar-nav">
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            {{-- Dashboard Link --}}
+            <li class="nav-item">
+                <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse"
+                    href="index.html">
+                    <i class="fa fa-home" aria-hidden="true"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            {{-- Data Barang Link --}}
+            <li class="nav-item">
+                <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse"
+                    href="#">
+                    <i class="bi bi-menu-button-wide"></i><span>Data Barang</span><i
+                        class="bi bi-chevron-down ms-auto"></i>
+                </a>
+            </li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            <!--===== PASIEN =======-->
+            {{-- Data Pasien Link --}}
+            <li class="nav-item">
+                <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse"
+                    href="#">
+                    <i class="bi bi-journal-text"></i><span>Data Pasien</span><i
+                        class="bi bi-chevron-down ms-auto"></i>
+                </a>
+            </li>
+            {{-- Diagnosis Pasien Link --}}
+            <li class="nav-item">
+                <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse"
+                    href="#">
+                    <i class="bi bi-journal-text"></i><span>Pemeriksaan Pasien</span><i
+                        class="bi bi-chevron-down ms-auto"></i>
+                </a>
+            </li>
+            {{-- Riwayat Pemeriksaan Pasien Link --}}
+            <li class="nav-item">
+                <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse"
+                    href="#">
+                    <i class="bi bi-journal-text"></i><span>Riwayat Pemeriksaan Pasien</span><i
+                        class="bi bi-chevron-down ms-auto"></i>
+                </a>
+            </li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            <!--===== TRANSAKSI =====-->
+            {{-- Pembayaran Tagihan Link --}}
+            <li class="nav-item">
+                <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse"
+                    href="#">
+                    <i class="bi bi-gem"></i><span>Pembayaran Tagihan</span><i
+                        class="bi bi-chevron-down ms-auto"></i>
+                </a>
+            </li>
+            {{-- Pembelian Barang Link --}}
+            <li class="nav-item">
+                <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse"
+                    href="#">
+                    <i class="bi bi-gem"></i><span>Pembelian Barang</span><i
+                        class="bi bi-chevron-down ms-auto"></i>
+                </a>
+            </li>
+            {{-- Laporan Transaksi Harian Link --}}
+            <li class="nav-item">
+                <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse"
+                    href="#">
+                    <i class="bi bi-gem"></i><span>Laporan Transaksi Harian</span><i
+                        class="bi bi-chevron-down ms-auto"></i>
+                </a>
+            </li>
+            {{-- Laporan Transaksi bulanan Link --}}
+            <li class="nav-item">
+                <a class="nav-link collapsed" data-bs-target="icons-nav" data-bs-toggle="collapse"
+                    href="#">
+                    <i class="bi bi-gem"></i><span>Laporan Transaksi Bulanan</span><i
+                        class="bi bi-chevron-down ms-auto"></i>
+                </a>
+            </li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            {{-- Log Out Link ? --}}
+            <li class="nav-item">
+                <a class="nav-link collapsed" data-bs-target="icons-nav" data-bs-toggle="collapse"
+                    href="#">
+            </li>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+        </ul>
+    </div>
+</nav>
