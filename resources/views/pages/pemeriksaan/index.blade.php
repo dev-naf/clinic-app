cae322d95a27d222e28bde223f8437a0985193f0
dev-naf <ahnaf.fattah@outlook.com>
2023-06-13 10:55:30 +0700

    halaman pemeriksaan

--- resources/views/pages/pemeriksaan/index.blade.php
+++ resources/views/pages/pemeriksaan/index.blade.php
@@ -1,29 +1,30 @@
 @extends('layouts.main')
 
 @section('content')
+    <link rel="stylesheet" href="css/fitur.css">
 
-<link rel="stylesheet" href="css/fitur.css">
-
-<div class="card-body">
-    <div class="row mt-5">
-        <div>
-            <h3 class="mb-2">Pemeriksaan Pasien</h3>
+    <div class="card-body">
+        <div class="row mt-5">
+            <div>
+                <h3 class="mb-2">Pemeriksaan Pasien</h3>
+            </div>
         </div>
     </div>
-</div>
 
-{{-- dibuat kotakan, isinya kolom pencarian id pasien, posisi ditengah, kalau mau tambah ccs di fitur.css --}}
-<div class="container">
-    <div class="form-floating mb-3">
+    {{-- dibuat kotakan, isinya kolom pencarian id pasien, posisi ditengah, kalau mau tambah ccs di fitur.css --}}
+    <div class="container">
+        <main class="row">
+            <form action="#" method="GET" class="mt-5">
+                <div class="form-floating mt-5 mb-4 col-10">
+                    <input id="floatingInput" type="id_pasien" class="form-control" placeholder="ID Pasien">
+                    <label for="floatingInput">ID Pasien</label>
+                </div>
+                <div class="col-10 text-center">
+                    <button type="submit" class="addButton">Search</button>
+                </div>
+            </form>
+        </main>
 
-        <input id="floatingInput" type="id_pasien" class="form-control" placeholder="ID Pasien">
-        <label for="floatingInput">ID Pasien</label>
-            
+
     </div>
-                                
-    <button type="submit" class="addButton">Search</button>
-
-</div>
-
-
 @endsection
